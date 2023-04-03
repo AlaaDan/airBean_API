93b18b2ba56d62dc9586600e7461e1264a399566
williamdinh <hoangdinh2411@gmail.com>
2023-04-03 13:25:15 +0200

    skapa mappar till project, installera bibliotek som behover

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
-const expres = require('express')
+const express = require('express')
+const { json } = require('express')
 
 const app = express()
-=======
-const { json } = require('express')
-const express = require('express')
-const app  = express()
-
-
+app.use(express.json())
+app.use('/api', require('./routers/index.js'))
 app.use(json())
->>>>>>> de5177e (Code base added)
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+  console.log('The server listening on port ' + port)
+})
